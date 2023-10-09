a4136976ebcf8130d2516089ec24dd35b579bde1
NGU516 <cdm05162@naver.com>
2023-10-09 20:49:32 +0900

    보행자 검출 알고리즘 비교

--- video.cpp
+++ video.cpp
@@ -25,30 +25,14 @@ void onTrackbarSlide(int pos, void* userdata) {
     cap.set(cv::CAP_PROP_POS_FRAMES, current_frame);
 }
 
-void mouse_callback(int event, int x, int y, int flags, void* userdata) {
-    if (event == cv::EVENT_LBUTTONDOWN) {
-        roi_rect.x = x;
-        roi_rect.y = y;
-        selecting_roi = true;
-    }
-    else if (event == cv::EVENT_MOUSEMOVE && selecting_roi) {
-        roi_rect.width = x - roi_rect.x;
-        roi_rect.height = y - roi_rect.y;
-    }
-    else if (event == cv::EVENT_LBUTTONUP) {
-        roi_rect.width = x - roi_rect.x;
-        roi_rect.height = y - roi_rect.y;
-        selecting_roi = false;
-    }
-}
-
-void detectAndDrawObjects(cv::Mat& fgmask, cv::Mat& frame) {
+// fgmask(subframe) = 전경 마스크 영상
+void detectAndDrawObjects(cv::Mat& subframe, cv::Mat& frame) {
     // 객체 벡터 초기화
     detected_objects.clear();
 
-    // 객체 외곽 찾기
+    // 객체 외곽선 찾기(여러개의 외곽선을 동시에 추출: 벡터<벡터>)
     std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(fgmask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    cv::findContours(subframe, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
     // 외곽 기반 객체 검출
     for (const auto& contour : contours) {
@@ -56,13 +40,42 @@ void detectAndDrawObjects(cv::Mat& fgmask, cv::Mat& frame) {
         // 객체 박스처리
         cv::Rect object_rect = cv::boundingRect(contour);
 
+        int object_size = object_rect.width * object_rect.height;
+        
+        if(object_size > 1000)
+            // 객체 크기와 프레임 번호 출력(디버깅)
+            std::cout << "[" << current_frame << "] frame 객체 크기 : " << object_size << " (Width: " << object_rect.width << ", Height: " << object_rect.height << ")" << std::endl;
+        
         // 특정 크기 이상의 객체만 고려
-        if (object_rect.width * object_rect.height > 1000) {
-            detected_objects.push_back(object_rect);
+        if (object_size > 1000) {
+            detected_objects.emplace_back(object_rect);
             cv::rectangle(frame, object_rect, cv::Scalar(0, 0, 255), 2);
+        }
+
+        // 현재 프레임 번호 출력(디버깅)
+        cv::putText(frame, "Frame : " + std::to_string(current_frame), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 0, 0), 2);
+    }
+    // subframe에서 127(그림자) 이하인 부분을 지움
+    cv::threshold(subframe, subframe, 127, 255, cv::THRESH_BINARY);
+}
+
+// HOG 객체 검출
+void detectAndDrawObjectsHOG(cv::Mat& frame, cv::HOGDescriptor& hog) {
+    std::vector<cv::Rect> fullbody;
+    std::vector<double> weights; // 검출된 객체의 가중치
+    hog.detectMultiScale(frame, fullbody, weights, 0, cv::Size(8, 8), cv::Size(32, 32), 1.05, 2);
+
+    for (size_t i = 0; i < fullbody.size(); i++) {
+        cv::Rect person = fullbody[i];
+        double weight = weights[i];
+
+        // 일정 가중치 이상인 객체만 그리기
+        if (weight >= 0) {
+            cv::rectangle(frame, person, cv::Scalar(255, 0, 0), 2);
         }
     }
 }
+
 
 int main() {
     if (!cap.isOpened()) {
@@ -74,14 +87,25 @@ int main() {
 
     cv::namedWindow("frame");
     cv::createTrackbar("Frame", "frame", &current_frame, count_frame - 1, onTrackbarSlide);
-    cv::setMouseCallback("frame", mouse_callback);
 
-    // 히스토리 길이, 임계값, 그림자 검출 여부
-    pMOG2 = cv::createBackgroundSubtractorMOG2(500, 16);
+    // 히스토리 길이, 임계값, 그림자 검출 여부(배경 제거 객체)
+    pMOG2 = cv::createBackgroundSubtractorMOG2(500, 16, true);
+
+
+    // HOG 객체 검출기 초기화
+    cv::HOGDescriptor hog;
+    hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+
+    // 전신 검출용 Haar Cascade 분류기 초기화
+    cv::CascadeClassifier fullbody_cascade;
+    if (!fullbody_cascade.load("haarcascade_fullbody.xml")) {
+        std::cerr << "파일을 찾을 수 없습니다." << std::endl;
+        return -1;
+    }
 
     while (true) {
         // 'p'를 통해 실시간 영상 멈춤
-        char key = cv::waitKey(10);
+        char key = cv::waitKey(100);
         if (key == 'p' || key == 'P') {
             is_playing = !is_playing;
         }
@@ -104,6 +128,18 @@ int main() {
 
             // fgmask(subframe)에서 객체 검출
             detectAndDrawObjects(subframe, frame);
+
+             // 전신 검출
+            std::vector<cv::Rect> fullbody;
+            fullbody_cascade.detectMultiScale(frame, fullbody, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(30, 80));
+
+            for (const auto& person : fullbody) {
+                cv::rectangle(frame, person, cv::Scalar(0, 255, 0), 2);
+            }
+
+            // HOG 객체 검출
+            detectAndDrawObjectsHOG(frame, hog);
+            
         }
 
         cv::imshow("frame", frame);
